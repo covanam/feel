59ac1c9d454ea8549efa3822ea0c712b4d8e9095
Cao Nam <cvn249@gmail.com>
2020-02-18 12:21:24 +0700

    visual.py now display all d

--- visual.py
+++ visual.py
@@ -7,29 +7,12 @@ model = pickle.load(open('model/model', 'rb'))
 alpha = model.alpha
 d = model.d
 
-significant_d = [c for a, c in sorted(zip(alpha, d), key=lambda x: x[0])]
-
-countryside_colors = significant_d[-9:]
-metropolitian_colors = significant_d[:9]
-
-insignificant_d = [c for a, c in sorted(zip(alpha, d), key=lambda x: abs(x[0]))]
-neutral_colors = insignificant_d[:9]
-
-im = np.empty((300, 300, 3), np.uint8)
-for i, color in enumerate(countryside_colors):
-    r = i % 3
-    c = i // 3
-    im[100*r:100*(r+1), 100*c:100*(c+1)] = color
-cv2.imwrite('countryside_colors.png', im)
-
-for i, color in enumerate(metropolitian_colors):
-    r = i % 3
-    c = i // 3
-    im[100*r:100*(r+1), 100*c:100*(c+1)] = color
-cv2.imwrite('metropolitian_colors.png', im)
-
-for i, color in enumerate(neutral_colors):
-    r = i % 3
-    c = i // 3
-    im[100*r:100*(r+1), 100*c:100*(c+1)] = color
-cv2.imwrite('neutral_colors.png', im)
+d = [c for a, c in sorted(zip(alpha, d), key=lambda x: x[0], reverse=True)]
+print(len(d))
+im = np.empty((200, 1000, 3), np.uint8)
+for i, color in enumerate(d):
+    r = i // 10
+    c = i % 10
+    print(r, c)
+    im[100*r:100*(r+1), 100*c:100*(1+c)] = color
+cv2.imwrite('colors.png', im)
